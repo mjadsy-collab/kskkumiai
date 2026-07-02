d321389a58fb45b9c2d62a281eaa6befe92c30ee
mjadsy-collab <mjadsy@docomo.ne.jp>
2026-07-02 10:07:18 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
-document.getElementById("contactBtn").addEventListener("click", function() {
-  alert("Please contact us at example@email.com");
+
+
+console.log("script loaded");
+console.log(typeof gsap);
+console.log(typeof ScrollTrigger);
+
+const opening = document.querySelector(".opening");
+const heroContent = document.querySelector(".hero-content");
+const header = document.querySelector(".header");
+
+window.addEventListener("scroll", () => {
+  const y = window.scrollY;
+
+  let blackOpacity = 1 - y / 1000;
+  if (blackOpacity < 0) blackOpacity = 0;
+
+  opening.style.opacity = blackOpacity;
+
+  let textOpacity = (y - 60) / 160;
+  if (textOpacity < 0) textOpacity = 0;
+  if (textOpacity > 1) textOpacity = 1;
+
+  heroContent.style.opacity = textOpacity;
+
+});
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+ScrollTrigger.create({
+  trigger: ".hero-content",
+  start: "top 35%",
+  end: "+=1030",
+  pin: true,
+  pinSpacing: false,
+  markers: false
+
 });
